0bb8125acfcdd466ef2c53b8fd8f529250e2ccf6
Peteris Krisjanis <pecisk@gmail.com>
2012-07-20 16:23:00 +0300

    Working towards working WaveformDrawing widget.

--- src/waveformwidget.c
+++ src/waveformwidget.c
@@ -1,3 +1,22 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * waveformreader.h
+ * Copyright (C) 2012 Peteris Krisjanis <pecisk@gmail.com>
+ * 
+ * libwaveform is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libwaveform is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.";
+ */
+
 #include "waveformwidget.h"
 #include <gtk/gtk.h>
 #include <cairo.h>
@@ -13,6 +32,9 @@ struct _WaveformDrawingPrivate
 {
   // pointer to WaveformData model
   WaveformData *data;
+  // variables for width and height
+  int width;
+  int height;
 };
 
 G_DEFINE_TYPE (WaveformDrawing, waveform_drawing, GTK_TYPE_DRAWING_AREA);
@@ -21,6 +43,9 @@ static void
 waveform_drawing_init(WaveformDrawing *self)
 {
 self->priv = WAVEFORM_DRAWING_GET_PRIVATE (self);
+self->priv->width = -1;
+self->priv->height = -1;
+self->priv->data = NULL;
 }
 
 static void
@@ -39,7 +64,7 @@ waveform_drawing_class_init(WaveformDrawingClass *klass)
  *
  * Creates #WaveformDrawing which widget object of waveform drawing.
  *
- * Returns: (transfer full): The new #WaveformDrawing widget
+ * Returns: (transfer none): The new #WaveformDrawing widget
  *
  * Since: 0.1
  */
@@ -51,22 +76,23 @@ WaveformDrawing * waveform_drawing_new(void)
 
 gboolean waveform_drawing_draw(GtkWidget *widget, cairo_t *cr)
 {
-
+   g_message("drawing.");
 	// FIXME taking cairo context from given params segfaults
 	// temporary fix, geting cairo context from widget window
 	cr = gdk_cairo_create(gtk_widget_get_window(widget));
 
 	// FIXME get allocated dimensions
-	GtkAllocation *allocation = NULL;
-	gtk_widget_get_allocation(widget, allocation);
-	int width = allocation->width;
-	int height = allocation->height;
-
+	GtkAllocation allocation = {-1,-1,-1,-1};
+	gtk_widget_get_allocation(widget, &allocation);
+	int width = allocation.width;
+	int height = allocation.height;
+	g_message("Allocation: %i %i", width, height);
 	// initialise x coordinate
 	int x = 1;
 
 	// cairo stuff
 	cairo_set_line_width (cr, 0.1);
+	
 	cairo_set_source_rgb(cr, 0, 0, 0);
 	
 	// get data from widget
@@ -74,6 +100,7 @@ gboolean waveform_drawing_draw(GtkWidget *widget, cairo_t *cr)
 	
 	// if there's WaveformData to draw, let's do it
 	if(self->priv->data != NULL) {
+		g_message("Data is available.");
 		// get data model
 		WaveformData *data_model = WAVEFORM_DATA(self->priv->data);
 		// get linked list of data
--- src/waveformwidget.h
+++ src/waveformwidget.h
@@ -1,3 +1,22 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * waveformreader.h
+ * Copyright (C) 2012 Peteris Krisjanis <pecisk@gmail.com>
+ * 
+ * libwaveform is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libwaveform is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.";
+ */
+
 #ifndef __WAVEFORMWIDGET_H
 #define __WAVEFORMWIDGET_H
 
@@ -20,6 +39,14 @@ G_BEGIN_DECLS
 typedef struct _WaveformDrawing WaveformDrawing;
 typedef struct _WaveformDrawingClass WaveformDrawingClass;
 typedef struct _WaveformDrawingPrivate WaveformDrawingPrivate;
+
+/**
+ * WaveformDrawing:
+ *
+ * The #WaveformDrawing structure.
+ *
+ * Since: 0.1
+ **/
 
 struct _WaveformDrawing {
   GtkDrawingArea parent;
