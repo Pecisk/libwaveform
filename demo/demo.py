8602c2ed863c1b942bb1e9c637ba8d029b4cef17
Peteris Krisjanis <pecisk@gmail.com>
2012-08-19 19:13:54 +0300

    Added python demo application, it has exception handling, and generic waveform widget creation and displaying.

---
+++ demo/demo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from gi.repository import Waveform, Gtk, GObject
+reader = Waveform.Reader.new()
+data = Waveform.Data.new()
+try:
+	levels = reader.get_levels("file:///home/user/test.wav", 120000000, 0, 0)
+except GObject.GError, e:
+	if e.domain == Waveform.ReaderError and e.code == Waveform.ReaderError.FILE:
+		print "File path is wrong, please correct it."
+	
+sublevels = reader.get_levels("file:///home/peteris/test.wav", 40000000, 0, 120000000)	
+
+data.add(levels)
+data.add(sublevels)
+win = Gtk.Window()
+scrolwin = Gtk.ScrolledWindow()
+waveform = Waveform.Drawing.new()
+win.set_size_request(400, 350)
+win.add(scrolwin)
+waveform.set_size_request(800,300)
+scrolwin.add_with_viewport(waveform)
+waveform.set_model(data)
+scrolwin.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
