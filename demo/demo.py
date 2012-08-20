058d2934fe9a211d55e1c23e6b844fb31b075a95
Peteris Krisjanis <pecisk@gmail.com>
2012-08-20 21:59:05 +0300

    Modified demo and added comment to README about it.

--- demo/demo.py
+++ demo/demo.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 
 from gi.repository import Waveform, Gtk, GObject
+import sys
+
 reader = Waveform.Reader.new()
 data = Waveform.Data.new()
+
+# Try to read 
 try:
 	levels = reader.get_levels("file:///home/user/test.wav", 120000000, 0, 0)
 except GObject.GError, e:
 	if e.domain == Waveform.ReaderError and e.code == Waveform.ReaderError.FILE:
 		print "File path is wrong, please correct it."
+		sys.exit()
 	
-sublevels = reader.get_levels("file:///home/peteris/test.wav", 40000000, 0, 120000000)	
+sublevels = reader.get_levels("file:///home/user/test.wav", 40000000, 0, 120000000)	
 
 data.add(levels)
 data.add(sublevels)
